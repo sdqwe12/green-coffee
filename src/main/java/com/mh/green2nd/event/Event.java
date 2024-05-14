8a67bb188eeaacfec6e93eceebdb4d4f01b9094c
dron512 <parkmyounghoi@gmail.com>
2024-05-14 16:29:30 +0900

    0514

--- src/main/java/com/mh/green2nd/event/Event.java
+++ src/main/java/com/mh/green2nd/event/Event.java
@@ -18,6 +18,7 @@ public class Event {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long eventId;
+
     @Column(name = "eventimgurl", nullable = true)
     @Schema(title = "이벤트상세이미지url주소",description = "이벤트상세이미지url주소")
     private String eventimgurl;
