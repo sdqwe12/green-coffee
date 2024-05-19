41735717b8c0559ba1d66082f86a08230dba313e
WIN-BVOJTL3EL22\jjj <jungjc1117@gmail.com>
2024-05-19 18:27:56 +0900

    240519 recommend

--- src/main/java/com/mh/green2nd/recommend/RecommendController.java
+++ src/main/java/com/mh/green2nd/recommend/RecommendController.java
@@ -15,9 +15,20 @@ import java.util.List;
 public class RecommendController {
     private final RecommendService recommendService;
 
-    @GetMapping("/")
-    public ResponseEntity<List<RecommendDto>> getRecommendations() {
-        List<RecommendDto> recommendDtoList = recommendService.getRecommendations();
-        return ResponseEntity.status(HttpStatus.OK).body(recommendDtoList);
+    // 추천 메뉴 리스트 조회
+    @GetMapping("/list")
+    public ResponseEntity<List<Recommend>> getRecommendList() {
+        List<Recommend> recommendList = recommendService.getRecommendList();
+        return new ResponseEntity<>(recommendList, HttpStatus.OK);
     }
+
+    // 추천 메뉴 상세 조회
+    @GetMapping("/detail")
+    public ResponseEntity<Recommend> getRecommendDetail(Long recommendId) {
+        Recommend recommend = recommendService.getRecommendDetail(recommendId);
+        return new ResponseEntity<>(recommend, HttpStatus.OK);
+    }
+
+
+
 }
