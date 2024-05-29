ee79e01564e2b916c833cc7937e9953b1b8eab51
JIN\dudgh <dudghcjswo3@gmail.com>
2024-05-29 16:50:47 +0900

    31 61611

--- src/main/java/com/mh/green2nd/orders/OrderState.java
+++ src/main/java/com/mh/green2nd/orders/OrderState.java
@@ -1,9 +1,9 @@
 package com.mh.green2nd.orders;
 
 public enum OrderState {
+    접수,
     주문승인,
     요청,
-    접수,
     요리중,
     준비완료
 }
--- src/main/java/com/mh/green2nd/orders/adminOrder/AdminOrderController.java
+++ src/main/java/com/mh/green2nd/orders/adminOrder/AdminOrderController.java
@@ -19,7 +19,7 @@ public class AdminOrderController {
     private final AdminOrderService adminOrderService;
 
     // 주문 상태 변경
-    @Operation(summary = "주문 상태 변경", description = "주문승인, 요청, 접수, 요리중, 준비완료 중 택1")
+    @Operation(summary = "주문 상태 변경", description = "접수, 주문승인, 요청, 요리중, 준비완료 중 택1")
     @PutMapping("/{orderId}/state")
     public ResponseEntity<String> updateOrderState(Long orderId, OrderState newState, Authentication authentication) {
         User currentUser = (User) authentication.getPrincipal();
